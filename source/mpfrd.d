c409f525ede50edd91e85c2ff38e02466ff4c878
Guillaume Chatelet <chatelet.guillaume@gmail.com>
2016-12-23 00:07:11 +0100

    Add helpers for mpfr

--- source/mpfrd.d
+++ source/mpfrd.d
@@ -17,35 +17,70 @@ struct Mpfr {
         mpfr_clear(mpfr);
     }
 
-    ref Mpfr opAssign(T)(T value) if(isNumeric!T) {
-        static if (is(T == double)) {
-            mpfr_set_d(mpfr, value, mpfr_rnd_t.MPFR_RNDN);
-        } else static if (is(T == float)) {
-            mpfr_set_flt(mpfr, value, mpfr_rnd_t.MPFR_RNDN);
-        } else static if (isIntegral!T && isSigned!T) {
-            mpfr_set_si(mpfr, value, mpfr_rnd_t.MPFR_RNDN);
+    private static string getTypeString(T)() {
+        static if (isIntegral!T && isSigned!T) {
+            return "_si";
         } else static if (isIntegral!T && !isSigned!T) {
-            mpfr_set_ui(mpfr, value, mpfr_rnd_t.MPFR_RNDN);
+            return "_ui";
+        } else static if (is(T : double)) {
+            return "_d";
+        } else static if (is(T == Mpfr)) {
+            return "";
         } else {
-            static assert(false, "Unhandled type "~T.stringof);
+            static assert(false, "Unhandled type " ~ T.stringof);
         }
+    }
+
+    // Assign
+
+    ref Mpfr opAssign(T)(T value) if(isNumeric!T) {
+        mixin("mpfr_set" ~ getTypeString!T() ~ "(mpfr, value, mpfr_rnd_t.MPFR_RNDN);");
+        return this;
+    }
+    
+    ref Mpfr opAssign(ref const Mpfr value) {
+        mpfr_set(mpfr, value, mpfr_rnd_t.MPFR_RNDN);
+        return this;
+    }
+    
+    // Compare and equals
+
+    int opCmp(T)(T value) const if(isNumeric!T) {
+        mixin("return mpfr_cmp" ~ getTypeString!T() ~ "(mpfr, value);");
+    }
+    
+    int opCmp(ref const Mpfr value) {
+        return mpfr_cmp(mpfr, value);
+    }
+
+    bool opEquals(T)(T value) const if(isNumeric!T) {
+        return opCmp(value) == 0;
+    }
+    
+    bool opEquals(ref const Mpfr value) {
+        return opCmp(value) == 0;
+    }
+    
+    // Arithmetic
+    private static string getOperatorString(string op)() {
+        final switch(op) {
+            case "+": return "_add";
+            case "-": return "_sub";
+            case "*": return "_mul";
+            case "/": return "_div";
+            case "^^": return "_pow";
+        }
+    }
+
+    ref Mpfr opOpAssign(string op, T)(T value) if(isNumeric!T) {
+        static assert(!(op == "^^" && isFloatingPoint!T), "no operator ^^= with floating point.");
+        mixin("mpfr" ~ getOperatorString!op() ~ getTypeString!T() ~ "(mpfr, mpfr, value, mpfr_rnd_t.MPFR_RNDN);");
         return this;
     }
 
-    int opCmp(T)(T value) if(isNumeric!T) {
-        static if (is(T : double)) {
-            return mpfr_cmp_d(mpfr, value);
-        } else static if (isIntegral!T && isSigned!T) {
-            return mpfr_cmp_si(mpfr, value);
-        } else static if (isIntegral!T && !isSigned!T) {
-            return mpfr_cmp_ui(mpfr, value);
-        } else {
-            static assert(false, "Unhandled type "~T.stringof);
-        }
-    }
-
-    bool opEquals(T)(T value) if(isNumeric!T) {
-        return opCmp(value) == 0;
+    ref Mpfr opOpAssign(string op)(ref const Mpfr value) {
+        mixin("mpfr" ~ getOperatorString!op() ~ "(mpfr, mpfr, value, mpfr_rnd_t.MPFR_RNDN);");
+        return this;
     }
 }
 
@@ -56,20 +91,80 @@ version (unittest)
 }
 
 unittest {
-    auto a = Mpfr(8);
+    auto value = Mpfr(8);
     foreach(T ; AliasSeq!(ubyte, ushort, uint, ulong, float, double, byte, short, int, long)) {
-        static if (isSigned!T) {
-            a = T(-1);
-            assert(a == T(-1));
-            assert(a <= T(-1));
-            assert(a > T(-2));
-            assert(a < T(0));
-        } else {
-            a = T(1);
-            assert(a == T(1));
-            assert(a <= T(1));
-            assert(a < T(2));
-            assert(a > T(0));
+        {
+            value = T(1);
+            assert(value == T(1));
+            assert(value <= T(1));
+            assert(value < T(2));
+            assert(value > T(0));
+            value *= T(2);
+            assert(value == T(2));
+            value /= T(2);
+            assert(value == T(1));
+            value += T(1);
+            assert(value == T(2));
+            value -= T(1);
+            assert(value == T(1));
         }
+        static if (!isFloatingPoint!T) {
+            value = T(2);
+            value ^^= T(2);
+            assert(value == T(4));
+        }
+        static if (isSigned!T) {
+            value = T(-1);
+            assert(value == T(-1));
+            assert(value <= T(-1));
+            assert(value > T(-2));
+            assert(value < T(0));
+        }
+    }
+    {
+        value = -1;
+        auto tmp = Mpfr(32);
+        tmp = -1;
+        assert(value == tmp);
+        assert(value <= tmp);
+        tmp = -2;
+        assert(value > tmp);
+        tmp = 0;
+        assert(value < tmp);
+        //
+        value = 2;
+        tmp = 2;
+        value *= tmp;
+        assert(value == 4);
+        //
+        value = 2;
+        tmp = 2;
+        value /= tmp;
+        assert(value == 1);
+        //
+        tmp = 1;
+        value = 2;
+        value += tmp;
+        assert(value == 3);
+        //
+        tmp = 1;
+        value = 2;
+        value -= tmp;
+        assert(value == 1);
+        //
+        tmp = 1;
+        value = 2;
+        value *= tmp;
+        assert(value == 2);
+        //
+        tmp = 2;
+        value = 2;
+        value /= tmp;
+        assert(value == 1);
+        //
+        tmp = 2;
+        value = 2;
+        value ^^= tmp;
+        assert(value == 4);
     }
 }
